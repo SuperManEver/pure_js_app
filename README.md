b72614bd8df4d3a6a3bdf975d69a468fa66b7a50
Nik Luparev <nikita.luparev@gmail.com>
2021-11-20 01:58:57 +0500

    page stub

---
+++ README.md
@@ -0,0 +1,8 @@
+## How to run app
+
+```sh
+yarn install
+yarn dev
+```
+
+Go to [app](http://localhost:1234) in browser
---
+++ src/app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Аналог $(document).ready(function(){
+  // Если должен быть найден один элемент
+  //   if (
+  //     (e = document.querySelector(
+  //       "#form_error_message_frontend + div > div:last-child label"
+  //     )) !== null
+  //   )
+  //     e.classList.add("last"); // Аналог выборки и присвоения класса
+  //   // Если элементов будет много
+  //   Array.prototype.forEach.call(
+  //     document.querySelectorAll(
+  //       "#form_error_message_frontend + div > div:last-child label"
+  //     ),
+  //     function (e) {
+  //       e.classList.add("last");
+  //     }
+  //   );
+
+  console.log("TEST");
+});
