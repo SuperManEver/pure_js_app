17a8c71bb30635ed688786233b4fc1ba49cbc3e1
Nikita Luparev <nikita.luparev@gmail.com>
2021-11-20 19:56:03 +0500

    refactor a little

---
+++ src/api.js
@@ -0,0 +1,55 @@
+const LANGUAGES = [
+  {
+    name: "Rust",
+    description:
+      "A language empowering everyone to build reliable and efficient software.",
+  },
+  {
+    name: "Rust",
+    description:
+      "A language empowering everyone to build reliable and efficient software.",
+  },
+  {
+    name: "Rust",
+    description:
+      "A language empowering everyone to build reliable and efficient software.",
+  },
+  {
+    name: "Rust",
+    description:
+      "A language empowering everyone to build reliable and efficient software.",
+  },
+  {
+    name: "Rust",
+    description:
+      "A language empowering everyone to build reliable and efficient software.",
+  },
+  {
+    name: "Rust",
+    description:
+      "A language empowering everyone to build reliable and efficient software.",
+  },
+  {
+    name: "Rust",
+    description:
+      "A language empowering everyone to build reliable and efficient software.",
+  },
+  {
+    name: "Rust",
+    description:
+      "A language empowering everyone to build reliable and efficient software.",
+  },
+];
+
+function getDelay() {
+  const MIN_NUMBER = 300;
+  const MAX_NUMBER = 600;
+
+  return Math.floor(Math.random() * MAX_NUMBER) + MIN_NUMBER;
+}
+
+export function loadItem() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(LANGUAGES), getDelay());
+  });
+}
--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
+import { loadItem } from "./api";
 import { add } from "./utils.js";
+
+// components
 import ListItem from "./list-item/index.js";
 
 const VALUES = ["ONE", "TWO", "THREE"];
 
-document.addEventListener("DOMContentLoaded", function () {
+let toggleMore = null;
+
+function handleLoadMore() {
+  console.log("load more");
+}
+
+function init() {
   const root = document.querySelector("#root");
 
+  toggleMore = document.querySelector(".load-more");
+
   if (!root) return;
+
+  if (toggleMore) {
+    toggleMore.addEventListener("click", handleLoadMore);
+  }
 
   VALUES.forEach((val) => {
     root.appendChild(ListItem({ name: val }));
   });
-});
+}
+
+function cleanUp() {
+  toggleMore.removeEventListener("click", handleLoadMore);
+}
+
+document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("beforeunload", cleanUp);
