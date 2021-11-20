8284df8893679609a59a77a0aaf55220ef9c1b14
Nikita Luparev <nikita.luparev@gmail.com>
2021-11-20 19:14:06 +0500

    add header

--- src/app.js
+++ src/app.js
@@ -4,24 +4,6 @@ import ListItem from "./list-item/index.js";
 const VALUES = ["ONE", "TWO", "THREE"];
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Аналог $(document).ready(function(){
-  // Если должен быть найден один элемент
-  //   if (
-  //     (e = document.querySelector(
-  //       "#form_error_message_frontend + div > div:last-child label"
-  //     )) !== null
-  //   )
-  //     e.classList.add("last"); // Аналог выборки и присвоения класса
-  //   // Если элементов будет много
-  //   Array.prototype.forEach.call(
-  //     document.querySelectorAll(
-  //       "#form_error_message_frontend + div > div:last-child label"
-  //     ),
-  //     function (e) {
-  //       e.classList.add("last");
-  //     }
-  //   );
-
   const root = document.querySelector("#root");
 
   if (!root) return;
