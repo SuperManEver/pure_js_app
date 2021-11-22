4d8c14cd846c6e12322064751f759163a923d743
Nik Luparev <nikita.luparev@gmail.com>
2021-11-22 23:12:08 +0500

    fixed 'this' issue

--- src/app.js
+++ src/app.js
@@ -4,45 +4,6 @@ import { render, createElement } from './utils/ui-builder'
 // components
 import ListItem from './components/list-item/index.js'
 
-let toggleMore = null
-const fetcher = charactersFetcher()
-let characters = []
-
-// async function handleLoadMore() {
-//   const items = await fetcher.getMore()
-
-//   characters = [...characters, ...items]
-
-//   renderView(characters)
-// }
-
-// function renderView(items) {
-//   const root = document.querySelector('#root')
-
-//   if (!root) return
-
-//   root.replaceChildren()
-//   const container = createElement('div', null, ...items.map(ListItem))
-
-//   render(container, root)
-// }
-
-// async function init() {
-//   toggleMore = document.querySelector('.load-more')
-
-//   if (toggleMore) {
-//     toggleMore.addEventListener('click', handleLoadMore)
-//   }
-
-//   characters = await fetcher.getInitial()
-
-//   renderView(characters)
-// }
-
-// function cleanUp() {
-//   toggleMore.removeEventListener('click', handleLoadMore)
-// }
-
 class App {
   constructor() {
     this.items = []
@@ -54,10 +15,10 @@ class App {
     this.root = document.querySelector('#root')
 
     if (this.toggleMore) {
-      this.toggleMore.addEventListener('click', this.handleLoadMore)
+      this.toggleMore.addEventListener('click', this.handleLoadMore.bind(this))
     }
 
-    this.items = await fetcher.getInitial()
+    this.items = await this.fetcher.getInitial()
 
     this.render()
   }
@@ -67,11 +28,11 @@ class App {
   }
 
   async handleLoadMore() {
-    const items = await fetcher.getMore()
+    const items = await this.fetcher.getMore()
 
-    this.items = [...characters, ...items]
+    this.items = [...this.items, ...items]
 
-    this.render(characters)
+    this.render()
   }
 
   render() {
@@ -86,5 +47,5 @@ class App {
 
 const app = new App()
 
-document.addEventListener('DOMContentLoaded', () => app.init())
-document.addEventListener('beforeunload', () => app.cleanUp())
+document.addEventListener('DOMContentLoaded', app.init.bind(app))
+document.addEventListener('beforeunload', app.cleanUp.bind(app))
