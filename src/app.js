8912d28fea42f569288e2338b99bfbdf6127b310
Nik Luparev <nikita.luparev@gmail.com>
2021-11-22 23:01:15 +0500

    move to classes

--- src/app.js
+++ src/app.js
@@ -8,40 +8,83 @@ let toggleMore = null
 const fetcher = charactersFetcher()
 let characters = []
 
-async function handleLoadMore() {
-  const items = await fetcher.getMore()
+// async function handleLoadMore() {
+//   const items = await fetcher.getMore()
 
-  characters = [...characters, ...items]
+//   characters = [...characters, ...items]
 
-  renderView(characters)
-}
+//   renderView(characters)
+// }
 
-function renderView(items) {
-  const root = document.querySelector('#root')
+// function renderView(items) {
+//   const root = document.querySelector('#root')
 
-  if (!root) return
+//   if (!root) return
 
-  root.replaceChildren()
-  const container = createElement('div', null, ...items.map(ListItem))
+//   root.replaceChildren()
+//   const container = createElement('div', null, ...items.map(ListItem))
 
-  render(container, root)
-}
+//   render(container, root)
+// }
 
-async function init() {
-  toggleMore = document.querySelector('.load-more')
+// async function init() {
+//   toggleMore = document.querySelector('.load-more')
 
-  if (toggleMore) {
-    toggleMore.addEventListener('click', handleLoadMore)
+//   if (toggleMore) {
+//     toggleMore.addEventListener('click', handleLoadMore)
+//   }
+
+//   characters = await fetcher.getInitial()
+
+//   renderView(characters)
+// }
+
+// function cleanUp() {
+//   toggleMore.removeEventListener('click', handleLoadMore)
+// }
+
+class App {
+  constructor() {
+    this.items = []
+    this.fetcher = charactersFetcher()
   }
 
-  characters = await fetcher.getInitial()
+  async init() {
+    this.toggleMore = document.querySelector('.load-more')
+    this.root = document.querySelector('#root')
 
-  renderView(characters)
+    if (this.toggleMore) {
+      this.toggleMore.addEventListener('click', this.handleLoadMore)
+    }
+
+    this.items = await fetcher.getInitial()
+
+    this.render()
+  }
+
+  cleanUp() {
+    this.toggleMore.removeEventListener('click', this.handleLoadMore)
+  }
+
+  async handleLoadMore() {
+    const items = await fetcher.getMore()
+
+    this.items = [...characters, ...items]
+
+    this.render(characters)
+  }
+
+  render() {
+    if (!this.root) return
+
+    this.root.replaceChildren()
+    const container = createElement('div', null, ...this.items.map(ListItem))
+
+    render(container, this.root)
+  }
 }
 
-function cleanUp() {
-  toggleMore.removeEventListener('click', handleLoadMore)
-}
+const app = new App()
 
-document.addEventListener('DOMContentLoaded', init)
-document.addEventListener('beforeunload', cleanUp)
+document.addEventListener('DOMContentLoaded', () => app.init())
+document.addEventListener('beforeunload', () => app.cleanUp())
